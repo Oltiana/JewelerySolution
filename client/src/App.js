ae636c5a90b37a10919ae100de1afe63f768fd4a
Loreta-Bl <loretabilalli8@gmail.com>
2025-11-19 10:50:30 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (LB)
+          Edit <code>src/App.js</code> and save to reload. (LB ma e mira)
         </p>
         <a
           className="App-link"
