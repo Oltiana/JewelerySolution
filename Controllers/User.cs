023a40091d583710dc964f3cd96a51a1ff732b75
erisasalihu <erisa.salihu03@gmail.com>
2026-01-24 23:41:31 +0100

    show all users

---
+++ Controllers/User.cs
@@ -0,0 +1,29 @@
+﻿using JewelerySolution.Data;
+using JewelerySolution.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JewelerySolution.Controllers
+{
+
+    public class UserController : Controller
+    {
+        private readonly JeweleryDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public UserController(JeweleryDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.Users
+                .ToList();
+
+            return View(users);
+        }
+
+    }
+}
